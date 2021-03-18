4f6108d15b76c4c92da546ace3ecc7dd06a8b265
Lucas Russo <lerwys@gmail.com>
2021-03-18 11:01:48 -0300

    afc-bpm: disable unused clocks from clock switch
    This might be causing some sort of conflict between
    LINK23_CLK and RTM_FPGA_CLK1, as both of them apparently
    connects to MGT116_CLK0.

--- port/board/afc-bpm/v3_1/adn4604_usercfg.h
+++ port/board/afc-bpm/v3_1/adn4604_usercfg.h
@@ -48,16 +48,16 @@
 #define ADN4604_EN_OUT_2        0       /* TCLKA_OUT */
 #define ADN4604_EN_OUT_3        0       /* TCLKB_OUT */
 #define ADN4604_EN_OUT_4        1       /* FPGA_CCLK */
-#define ADN4604_EN_OUT_5        1       /* FP2_CLK2 */
+#define ADN4604_EN_OUT_5        0       /* FP2_CLK2 */
 #define ADN4604_EN_OUT_6        1       /* LINK01_CLK */
 #define ADN4604_EN_OUT_7        1       /* FP2_CLK1 */
 #define ADN4604_EN_OUT_8        1       /* PCIE_CLK1 */
-#define ADN4604_EN_OUT_9        1       /* LINK23_CLK */
-#define ADN4604_EN_OUT_10       1       /* FIN1_CLK3 */
-#define ADN4604_EN_OUT_11       1       /* FIN1_CLK2 */
+#define ADN4604_EN_OUT_9        0       /* LINK23_CLK */
+#define ADN4604_EN_OUT_10       0       /* FIN1_CLK3 */
+#define ADN4604_EN_OUT_11       0       /* FIN1_CLK2 */
 #define ADN4604_EN_OUT_12       0       /* RTM_SYNC_CLK */
 #define ADN4604_EN_OUT_13       1       /* OP15C (Aux U-Fl connector) */
-#define ADN4604_EN_OUT_14       1       /* FIN2_CLK2 */
-#define ADN4604_EN_OUT_15       1       /* FIN2_CLK3 */
+#define ADN4604_EN_OUT_14       0       /* FIN2_CLK2 */
+#define ADN4604_EN_OUT_15       0       /* FIN2_CLK3 */
 
 #endif
